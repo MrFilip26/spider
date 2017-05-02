211a15b867aaba5b063865f42f0892969ba7c16a
Filip <mrfilip626@gmail.com>
2017-05-02 11:05:46 +0200

    FSA - one leg at time walk

--- spider_walk/spider_walk.ino
+++ spider_walk/spider_walk.ino
@@ -1,22 +1,18 @@
 #include "Arduino.h"
 #include "Servo.h"
 
-#define SPEED 10
-#define VM 35
-#define HM 50
-#define STEPDEG 10
-
+#define SPEED 30
+#define VM 10
+#define HM 40
+#define STEPDEG 15
 
 char cNEW;
-//35
 int vL1val = 130, vR1val = 120,
     vL2val = 130, vR2val = 120,
     vL3val = 120, vR3val = 120,
 
     hL1val = 95, hR1val = 85,
-    hL2val = 90, hR2val = 90,
-    hL3val = 70, hR3val = 115;
-
+    hL2val = 70, hR2val = 110;
 
 Servo //vertical
     vL1, vR1,
@@ -25,11 +21,9 @@ Servo //vertical
 
     //horizontal
     hL1, hR1,
-    hL2, hR2,
-    hL3, hR3;
+    hL2, hR2;
 
 void setup() {
-
   Serial.begin(9600); // init serial port
   
     vL1.attach(4);
@@ -39,14 +33,17 @@ void setup() {
     vL3.attach(8);
     vR3.attach(36);
 
-    hL1.attach(5);
+    hL1.attach(34);
     hR1.attach(30);
     hL2.attach(7);
     hR2.attach(26);
-    hL3.attach(9);
-    hR3.attach(34);
 
     initPosition();
+}
+void loop() {
+  initPosition();
+  handleBluetooth();
+  delay(350);
 }
 void handleBluetooth()
 {
@@ -57,10 +54,10 @@ void handleBluetooth()
         switch (cNEW)
         {
             case 'I': { initPosition(); break; }
-            case 'W': { stepF(); break; }
-            case 'S': { stepBack(); break; }
-            case 'A': { stepL(); break; }
-            case 'D': { stepR(); break; }
+            case 'W': { stepF(10); break; }
+            case 'S': { stepBack(20); break; }
+            case 'A': { stepL(15); break; }
+            case 'D': { stepR(15); break; }
         }
     }
 }
@@ -79,19 +76,16 @@ void initPosition()
   hR1.write(hR1val);
   hL2.write(hL2val);
   hR2.write(hR2val);
-  hL3.write(hL3val);
-  hR3.write(hR3val);
 }
 //inicializuje nohy do polohy na chodenie -- treba len ak nohy mozu byt v inej ako inicializovanej polohe
 void initStepF()
 {
-    vL1val = 120, vR1val = 120,
-    vL2val = 120, vR2val = 120,
+    vL1val = 130, vR1val = 120,
+    vL2val = 130, vR2val = 120,
     vL3val = 120, vR3val = 120,
 
-    hL1val = 110, hR1val = 70,
-    hL2val = 90, hR2val = 90,
-    hL3val = 70, hR3val = 115;
+    hL1val = 95, hR1val = 85,
+    hL2val = 60, hR2val = 120;
     
     vL1.write(vL1val);
     vL2.write(vL2val);
@@ -102,210 +96,291 @@ void initStepF()
     
     hL1.write(hL1val);
     hL2.write(hL2val);
-    hL3.write(hL3val);
     hR1.write(hR1val);
     hR2.write(hR2val);
-    hR3.write(hR3val);
     
     delay(100);
 }
 //posunie sa o jeden krok vpred
-void stepF()
+void stepF(int speedStep)
 {
   //Serial.println("StepF");
   for(int i = 0; i < HM; i++){
     hL1.write(++hL1val);
     hL2.write(++hL2val);
-    hL3.write(++hL3val);
+    
     hR1.write(--hR1val);
     hR2.write(--hR2val);
-    hR3.write(--hR3val);
-    delay(SPEED);
+    delay(3*speedStep);
   }
   for(int i = 0; i < VM; i++){
     vL2.write(--vL2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR1.write(--vR1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR3.write(--vR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < HM; i++){
     hL2.write(--hL2val);
     hR1.write(++hR1val);
-    hR3.write(++hR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vL2.write(++vL2val);
-    vR1.write(++vR1val);
-    vR3.write(++vR3val);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
+    vR1.write(++vR1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
+    vR3.write(++vR3val);
+    delay(speedStep);
+  }
+
+  for(int i = 0; i < VM; i++){
     vR2.write(--vR2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL1.write(--vL1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL3.write(--vL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < HM; i++){
     hR2.write(++hR2val);
     hL1.write(--hL1val);
-    hL3.write(--hL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vR2.write(++vR2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL1.write(++vL1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL3.write(++vL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
 }
 //posunie sa o jeden krok s5
-void stepBack()
+void stepBack(int speedStep)
 {
   //Serial.println("StepBack");
   for(int i = 0; i < HM; i++){
     hL1.write(--hL1val);
     hL2.write(--hL2val);
-    hL3.write(--hL3val);
     hR1.write(++hR1val);
     hR2.write(++hR2val);
-    hR3.write(++hR3val);
-    delay(SPEED);
+    delay(3*speedStep);
   }
   for(int i = 0; i < VM; i++){
     vL2.write(--vL2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR1.write(--vR1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR3.write(--vR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < HM; i++){
     hL2.write(++hL2val);
     hR1.write(--hR1val);
-    hR3.write(--hR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vL2.write(++vL2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR1.write(++vR1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR3.write(++vR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vR2.write(--vR2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL1.write(--vL1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL3.write(--vL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < HM; i++){
     hR2.write(--hR2val);
     hL1.write(++hL1val);
-    hL3.write(++hL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vR2.write(++vR2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL1.write(++vL1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL3.write(++vL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
 }
 //natoci sa o jeden krok vpravo
-void stepR()
+void stepR(int speedStep)
 {
   //Serial.println("TurnRight");
   for(int i = 0; i < HM; i++){
     hL1.write(++hL1val);
     hL2.write(++hL2val);
-    hL3.write(++hL3val);
     hR1.write(++hR1val);
     hR2.write(++hR2val);
-    hR3.write(++hR3val);
-    delay(SPEED);
+    delay(3*speedStep);
   }
   for(int i = 0; i < VM; i++){
     vL2.write(--vL2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR1.write(--vR1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR3.write(--vR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < HM; i++){
     hL2.write(--hL2val);
     hR1.write(--hR1val);
-    hR3.write(--hR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vL2.write(++vL2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR1.write(++vR1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR3.write(++vR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vR2.write(--vR2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL1.write(--vL1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL3.write(--vL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < HM; i++){
     hR2.write(--hR2val);
     hL1.write(--hL1val);
-    hL3.write(--hL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vR2.write(++vR2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL1.write(++vL1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL3.write(++vL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
 }
 //natoci sa o jeden krok vlavo
-void stepL()
+void stepL(int speedStep)
 {
   //Serial.println("TurnLeft");
   for(int i = 0; i < HM; i++){
     hL1.write(--hL1val);
     hL2.write(--hL2val);
-    hL3.write(--hL3val);
     hR1.write(--hR1val);
     hR2.write(--hR2val);
-    hR3.write(--hR3val);
-    delay(SPEED);
+    delay(3*speedStep);
   }
   for(int i = 0; i < VM; i++){
     vL2.write(--vL2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR1.write(--vR1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR3.write(--vR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < HM; i++){
     hL2.write(++hL2val);
     hR1.write(++hR1val);
-    hR3.write(++hR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vL2.write(++vL2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR1.write(++vR1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vR3.write(++vR3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vR2.write(--vR2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL1.write(--vL1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL3.write(--vL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < HM; i++){
     hR2.write(++hR2val);
     hL1.write(++hL1val);
-    hL3.write(++hL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
   for(int i = 0; i < VM; i++){
     vR2.write(++vR2val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL1.write(++vL1val);
+    delay(speedStep);
+  }
+  for(int i = 0; i < VM; i++){
     vL3.write(++vL3val);
-    delay(SPEED);
+    delay(speedStep);
   }
 }
 //zatoci o "deg" stupnov vpravo
@@ -313,7 +388,7 @@ void turnR(int deg)
 {
     for(int i = 0; i < deg; i+= STEPDEG)
     {
-      stepR;  
+      stepR(20);  
     }
 }
 //zatoci o "deg" stupnov vlavo
@@ -321,14 +396,6 @@ void turnL(int deg)
 {
     for(int i = 0; i < deg; i+= STEPDEG)
     {
-      stepL;  
+      stepL(30);  
     }
-}
-void loop() {
-  handleBluetooth();
-  //initPosition();
-  //stepR();
-  //stepL();
-  //stepF();
-  //stepBack()
 }
